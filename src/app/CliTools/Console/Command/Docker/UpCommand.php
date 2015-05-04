4116e260f5c802e4d8abc624e863557252b3e116
Markus Blaschke <mblaschke82@gmail.com>
2015-05-05 00:16:30 +0200

    Cleanup

--- src/app/CliTools/Console/Command/Docker/UpCommand.php
+++ src/app/CliTools/Console/Command/Docker/UpCommand.php
@@ -20,7 +20,6 @@ namespace CliTools\Console\Command\Docker;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use CliTools\Console\Builder\CommandBuilder;
