946f594befc4520300e431a75e72d4a51c32d28f
Markus Blaschke <mblaschke82@gmail.com>
2015-06-03 23:36:20 +0200

    Improved sync selection

--- src/app/CliTools/Console/Command/Sync/AbstractSyncCommand.php
+++ src/app/CliTools/Console/Command/Sync/AbstractSyncCommand.php
@@ -76,13 +76,66 @@ abstract class AbstractSyncCommand extends \CliTools\Console\Command\Sync\Abstra
                 throw new \RuntimeException('No valid servers found in configuration');
             }
 
-            $questionList = array_combine(array_values($serverList), array_values($serverList) );
-            $question = new ChoiceQuestion('Please choose process for tracing', $questionList);
-            $question->setMaxAttempts(1);
+            $serverOptionList = array();
 
-            $questionDialog = new QuestionHelper();
+            foreach ($serverList as $context) {
+                $line = array();
 
-            $ret = $questionDialog->ask($this->input, $this->output, $question);
+                // hostname
+                $optPath = $context . '.ssh.hostname';
+                if ($this->config->exists($optPath)) {
+                    $line[] = '<info>host:</info>' . $this->config->get($optPath);
+                }
+
+                // rsync path
+                $optPath = $context . '.rsync.path';
+                if ($this->config->exists($optPath)) {
+                    $line[] = '<info>rsync:</info>' . $this->config->get($optPath);
+                }
+
+                // mysql database list
+                $optPath = $context . '.mysql.database';
+                if ($this->config->exists($optPath)) {
+                    $dbList        = $this->config->getArray($optPath);
+                    $foreignDbList = array();
+
+                    foreach ($dbList as $databaseConf) {
+                        if (strpos($databaseConf, ':') !== false) {
+                            // local and foreign database in one string
+                            list($localDatabase, $foreignDatabase) = explode(':', $databaseConf, 2);
+                            $foreignDbList[] = $foreignDatabase;
+                        } else {
+                            // database equal
+                            $foreignDbList[] = $databaseConf;
+                        }
+                    }
+
+                    if (!empty($foreignDbList)) {
+                        $line[] .= '<info>mysql:</info>' . implode(', ', $foreignDbList);
+                    }
+                }
+
+                if (!empty($line)) {
+                    $line = implode(' ', $line);
+                } else {
+                    // fallback
+                    $line = $context;
+                }
+
+                $serverOptionList[$context] = $line;
+            }
+
+            try {
+                $question = new ChoiceQuestion('Please choose server context for synchronization', $serverOptionList);
+                $question->setMaxAttempts(1);
+
+                $questionDialog = new QuestionHelper();
+
+                $ret = $questionDialog->ask($this->input, $this->output, $question);
+            } catch(\InvalidArgumentException $e) {
+                // Invalid server context, just stop here
+                throw new \CliTools\Exception\StopException(1);
+            }
         } else {
             $ret = $this->input->getArgument('context');
         }
