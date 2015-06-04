c22292a5bc0ac87833701bdd8bc3058c5e94e5bf
Markus Blaschke <mblaschke82@gmail.com>
2015-06-04 21:13:06 +0200

    Version bump 1.9.910

--- src/command.php
+++ src/command.php
@@ -20,7 +20,7 @@
  */
 
 error_reporting(E_ALL);
-define('CLITOOLS_COMMAND_VERSION', '1.9.909');
+define('CLITOOLS_COMMAND_VERSION', '1.9.910');
 define('CLITOOLS_ROOT_FS', __DIR__);
 
 require __DIR__ . '/vendor/autoload.php';
